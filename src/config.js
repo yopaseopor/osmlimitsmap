51747faf32682e224e7a7bc17119183bdce722ec
yopaseopor <yopaseopor@gmail.com>
2022-06-25 21:51:48 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -789,7 +789,8 @@ var config = {
 							scale:0.07
 						}),
 						text: new ol.style.Text({
-							text: maxspeed
+							text: maxspeed,
+								font: 'small-caps bold 18px/1 sans-serif',
 						})
 					}));
 				}
