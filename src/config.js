1d690bbf0c0d3635c2d768924aa1b4fb1c6d1df8
yopaseopor <yopaseopor@gmail.com>
2022-07-25 15:59:23 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -755,7 +755,7 @@ var config = {
   },
 		{
 			group: 'Falta / Missing',
-			title: 'No tracktype3',
+			title: 'No tracktype4',
 			query: '(way["highway"="track"][!"tracktype"]({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'icones/maxaxleload_question.svg',
 			iconStyle: 'background-color:rgba(0,0,0,0.4)',
