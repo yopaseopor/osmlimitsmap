841b5edfe40eb93f85414f101e8de28ce911d4de
yopaseopor <yopaseopor@gmail.com>
2022-11-06 15:21:38 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -485,7 +485,79 @@ var config = {
 
 				return styles;
 			}
-},
+				},
+		{
+			group: 'Limits',
+			title: 'Incline Up',
+			query: '(nwr["highway"!="steps"]["incline"~"^.[0-9]%"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'accessibilitat/incline_up.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^incline$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'accessibilitat/incline_up.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		{
+			group: 'Limits',
+			title: 'Incline Down',
+			query: '(nwr["highway"!="steps"]["incline"~"^-.[0-9]%"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'accessibilitat/incline_down.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^incline$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'accessibilitat/incline_down.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
 		{
 			group: 'No limits',
 			title: 'No maxspeed',
