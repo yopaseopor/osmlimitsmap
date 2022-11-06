e3735f61d648a20610d82267c00f4d66ffd205a6
yopaseopor <yopaseopor@gmail.com>
2022-11-06 22:56:12 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -570,7 +570,7 @@ style: function (feature) {
 							text: new ol.style.Text({
 								text: maxspeed,
 								offsetX : -1,
-								offsetY : 2,
+								offsetY : 3,
 								rotation : 5.7,
 								fill: new ol.style.Fill({
                             color: 'rgba(0,0,0,1)'
