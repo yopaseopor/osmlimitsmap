44abd1d5d0df75243c830f829db772d2552483d2
yopaseopor <yopaseopor@gmail.com>
2022-11-08 21:28:26 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -488,7 +488,7 @@ var config = {
 				},
 		{
 			group: 'Limits',
-			title: 'Incline Upold',
+			title: 'Incline Upold2',
 			query: '(nwr["highway"!="steps"]["incline"~"^[0-9]."]({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'icones/incline_up.svg',
 			iconStyle: 'background-color:rgba(255,255,255,0.4)',
